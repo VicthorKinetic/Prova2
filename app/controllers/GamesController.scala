8bb4d14ca2294ffbed1fd361fa800e138475bf30
Victhor <kineticplayer9@gmail.com>
2018-11-27 00:28:00 +0000

    Atualização do GamesController para criação de formulário

--- app/controllers/GamesController.scala
+++ app/controllers/GamesController.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 import play.api.db.Database
 import scala.collection.mutable.MutableList
 import models.Games
+import models.GamesDAO
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.format.Formats._
@@ -17,30 +18,57 @@ import play.api.data.format.Formats._
 @Singleton
 class GamesController @Inject()(db: Database, cc: ControllerComponents) 
   extends AbstractController(cc) with play.api.i18n.I18nSupport {
-  
-  def games = Action {
- 
-    val list = MutableList[Games]()
-    //conn representa a conexao de fato com o bd
-    db.withConnection { conn =>
-      val stm = conn.createStatement()
-      val res = stm.executeQuery("""
-      select 
-         * 
-      from 
-         games 
-      order by 
-          games.nome 
-      limit 10""")
-      while (res.next()) {
-        list.+=(Games(res.getInt(1)
-                  ,res.getString(2)
-                  ,res.getString(3)
-                  ,res.getString(4)
-                  ,res.getString(5)
-                  ,res.getInt(6)))
+    
+  val form: Form[(String, String, String, String, Int)] = Form (
+        mapping(
+            "nome" -> text,
+            "desenvolvedor" -> text,
+            "genero" -> text,
+            "plataforma" -> text,
+            "ano" -> number
+        )(Games.applyGame)(Games.unapplyGame)
+    )
+    
+  def create = Action {implicit request =>
+    form.bindFromRequest.fold(
+      formWithErrors => {
+        println(formWithErrors)
+        BadRequest(views.html.games(formWithErrors))
+      },
+      games => {
+        GamesDAO.create(db,Games.criarGame(games._1,games._2,games._3,games._4,games._5))
+        Redirect("/games")
       }
-    }
+    )
+  }
+  
+  def formGames = Action {implicit request =>
+    Ok(views.html.games(form))
+  }
+ 
+    def games = Action {
+ 
+      val list = MutableList[Games]()
+      //conn representa a conexao de fato com o bd
+      db.withConnection { conn =>
+        val stm = conn.createStatement()
+        val res = stm.executeQuery("""
+        select 
+           * 
+        from 
+           games 
+        order by 
+            games.nome 
+        limit 10""")
+        while (res.next()) {
+          list.+=(Games(res.getInt(1)
+                    ,res.getString(2)
+                    ,res.getString(3)
+                    ,res.getString(4)
+                    ,res.getString(5)
+                    ,res.getInt(6)))
+        }
+      }
     Ok(views.html.listaGames(list))
   }
 }
