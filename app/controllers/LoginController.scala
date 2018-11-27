519aa0db613d2572b8bfe5c48ad60437ea982235
Victhor <kineticplayer9@gmail.com>
2018-11-27 01:20:17 +0000

    Criação do LoginController para login de usuários

---
+++ app/controllers/LoginController.scala
@@ -0,0 +1,52 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.db.Database
+import scala.collection.mutable.MutableList
+import models.GamesDAO
+import models.Login
+import play.api.data._
+import play.api.data.Forms._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class LoginController @Inject()(db: Database, cc: ControllerComponents) 
+  extends AbstractController(cc) with play.api.i18n.I18nSupport {
+  
+  val loginForm: Form[Login] = Form (
+        mapping(
+            "email" -> text,
+            "senha" -> text
+    )(Login.apply)(Login.unapply))
+  
+  def form = Action {implicit request =>
+    Ok(views.html.loginForm(loginForm))
+  }
+  
+  def auth = Action {implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.loginForm(formWithErrors))
+      },
+      login => {
+        val estaLogado = GamesDAO.autenticar(db,login)
+        if(estaLogado){
+           Redirect("/").withSession("games" -> login.email)   
+        }else{
+           Redirect("/login")
+        }
+      }
+    )
+  }
+  
+  def logout = Action {implicit request =>
+    Redirect("/").withSession(request.session - "games")
+  }
+
+  
+}
